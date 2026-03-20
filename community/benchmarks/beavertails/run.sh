f074eaed442d93671ac9cc8521dc5aae481c0807
Filip Sondej <26285707+filyp@users.noreply.github.com>
2026-03-20 13:25:10 +0100

    v4 beaver_tails

--- community/benchmarks/beavertails/run.sh
+++ community/benchmarks/beavertails/run.sh
@@ -5,8 +5,15 @@
 
 # note, experiments were done with adamw_8bit as the default optimizer in finetune.yaml
 
-common="sbatch runners/slurm_runner.sh python src/unlearn_relearn.py --config-name=unlearn.yaml --multirun experiment=unlearn/beavertails/curated"
-reference="sbatch runners/slurm_runner.sh python src/unlearn_relearn.py --config-name=unlearn.yaml experiment=unlearn/beavertails/curated trainer.args.num_train_epochs=0"
+common="python src/unlearn_relearn.py --config-name=unlearn.yaml --multirun experiment=unlearn/beavertails/curated"
+reference="python src/unlearn_relearn.py --config-name=unlearn.yaml experiment=unlearn/beavertails/curated trainer.args.num_train_epochs=0"
+
+# Auto-detect if we're on SLURM
+if command -v sbatch &> /dev/null; then
+    echo "Running on SLURM"
+    common="sbatch runners/slurm_runner.sh $common"
+    reference="sbatch runners/slurm_runner.sh $reference"
+fi
 
 # model=gemma-3-270m
 # model=gemma-3-4b-pt
@@ -15,10 +22,11 @@ model=gemma-2-2b
 category='animal_abuse'
 # category='terrorism,organized_crime'
 
-version=v3
+version=v4
 # "no version" used the original beavertails dataset, where there is data duplication and mislabeling; also, it wasn't finished, got terminated before 50 trials
 # v2 uses our curated high-quality subset
 # v3 uses a 2x smaller LR during relearning, because it was too severe
+# v4 tunes probability, not loss; also npo saturation was removed from repselect
 
 ${reference} model=${model} category=${category} trainer=GradDiff task_name=${version}_${model}_${category}_reference
 ${common} model=${model} category=${category} trainer=RepSelect task_name=${version}_${model}_${category}_RepSelect
@@ -27,38 +35,3 @@ ${common} model=${model} category=${category} trainer=NPO task_name=${version}_$
 ${common} model=${model} category=${category} trainer=RMU task_name=${version}_${model}_${category}_RMU
 ${common} model=${model} category=${category} trainer=SimNPO task_name=${version}_${model}_${category}_SimNPO
 ${common} model=${model} category=${category} trainer=UNDIAL task_name=${version}_${model}_${category}_UNDIAL
-
-
-
-# common="python3 src/unlearn_relearn.py \
-# --multirun \
-# --config-name=unlearn.yaml \
-# experiment=unlearn/wmdp_low_mi/default"
-
-# ver=v4
-
-
-# # Auto-detect if we're on SLURM
-# if command -v sbatch &> /dev/null; then
-#     echo "Running on SLURM"
-#     common="sbatch $HOME/open-unlearning/runners/slurm_runner.sh $common"
-# fi
-
-
-# $common model=Llama-3.2-3B trainer=GradDiff task_name=${ver}_3B_GradDiff_bio
-# $common model=Llama-3.2-3B trainer=NPO task_name=${ver}_3B_NPO_bio
-# $common model=Llama-3.2-3B trainer=RMU task_name=${ver}_3B_RMU_bio
-# $common model=Llama-3.2-3B trainer=SimNPO task_name=${ver}_3B_SimNPO_bio
-# $common model=Llama-3.2-3B trainer=UNDIAL task_name=${ver}_3B_UNDIAL_bio
-# $common model=Llama-3.2-3B trainer=RepSelect task_name=${ver}_3B_RepSelect_bio
-# $common model=Llama-3.2-3B trainer=RepSelect task_name=${ver}_3B_RepSelectstrict_bio eval.wikitext_kl.disr_budget=0.005
-# $common model=Llama-3.2-3B trainer=NPO task_name=${ver}_3B_NPOstrict_bio eval.wikitext_kl.disr_budget=0.005
-
-# $common model=Llama-3.2-3B trainer=GradDiff task_name=${ver}_3B_GradDiff_cyber wmdp_domain=cyber
-# $common model=Llama-3.2-3B trainer=NPO task_name=${ver}_3B_NPO_cyber wmdp_domain=cyber
-# $common model=Llama-3.2-3B trainer=RMU task_name=${ver}_3B_RMU_cyber wmdp_domain=cyber
-# $common model=Llama-3.2-3B trainer=SimNPO task_name=${ver}_3B_SimNPO_cyber wmdp_domain=cyber
-# $common model=Llama-3.2-3B trainer=UNDIAL task_name=${ver}_3B_UNDIAL_cyber wmdp_domain=cyber
-# $common model=Llama-3.2-3B trainer=RepSelect task_name=${ver}_3B_RepSelect_cyber wmdp_domain=cyber
-# $common model=Llama-3.2-3B trainer=RepSelect task_name=${ver}_3B_RepSelectstrict_cyber eval.wikitext_kl.disr_budget=0.005 wmdp_domain=cyber
-# $common model=Llama-3.2-3B trainer=NPO task_name=${ver}_3B_NPOstrict_cyber eval.wikitext_kl.disr_budget=0.005 wmdp_domain=cyber
